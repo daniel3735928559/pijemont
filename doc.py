1e52264c3628aae7da46a316b69a5a29497d83a1
DR <daniel3735928559@gmail.com>
2016-06-06 16:21:27 -0500

    doc gen improvements

--- doc.py
+++ doc.py
@@ -1,8 +1,13 @@
-import urllib2, json, sys
+import json, sys, yaml
 
-def print_docs(api_url):
-    api = json.loads(urllib2.urlopen(api_url).read())['api']
+def print_docs_yaml(filename):
+    with open(filename) as f:
+        api = yaml.load(f.read())
     print(doc_gen(api))
+
+# def print_docs(api_url):
+#     api = json.loads(urllib2.urlopen(api_url).read())['api']
+#     print(doc_gen(api))
     
 def doc_gen(api):
     return "\n\n".join(["###`{func}({shortargs})`\n\n{desc}\n\n####Arguments:\n{longargs}".format(
@@ -27,9 +32,9 @@ def args_gen(api, depth):
                                                                     value=args_gen(api['values'][k], depth+1),
                                                                     desc=("\n    "+indent+api['values'][k]['description'] if 'description' in api['values'][k] else ""))
                                                                     for k in api['values']]))
-    elif(api["type"] == "string" and "values" in api and len(api['values'])>0):
+    elif(api["type"] == "str" and "values" in api and len(api['values'])>0):
         return "`"+" | ".join(["\"" + k + "\"" for k in api["values"]])+"`"
-    if(api["type"] == "string" or api["type"] == "multiline"):
+    if(api["type"] == "str" or api["type"] == "multiline"):
         return "`string`"
     elif(api["type"] == "num"):
         return "`num`"
@@ -37,5 +42,8 @@ def args_gen(api, depth):
         return "`file`"
     elif(api["type"] == "oneof"):
         return " | ".join([args_gen(api['values'][k], depth+1) for k in api["values"]])
+    else:
+        return "`{type}`".format(type=api["type"])
 
-print_docs(sys.argv[1])
+#print_docs(sys.argv[1])
+print_docs_yaml(sys.argv[1])
